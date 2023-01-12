75700a7bb3a14149989d85abedcea4f71c764ae7
bokorzita <104830913+bokorzita@users.noreply.github.com>
2023-01-12 01:44:09 +0100

    singleton

---
+++ singleton.js
@@ -0,0 +1,35 @@
+//creational
+
+//example 1:
+function Process(state) {
+  this.state = state
+}
+
+const Singleton = (function(){
+  function ProcessManager() {
+    this.numProcess = 0
+  }
+
+  let pManager
+
+  function createProcessManager() {
+    pManager = new ProcessManager()
+    return pManager
+  }
+
+  return {
+    getProcessManager: () => {
+      if(!pManager)
+      pManager = createProcessManager()
+      return pManager
+    }
+  }
+})()
+
+const processManager = Singleton.getProcessManager()
+const processManager2 = Singleton.getProcessManager()
+
+console.log(processManager === processManager2)
+
+//example 2:
+//see singleton.html
